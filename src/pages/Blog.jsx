87db02ef57d4f01baaf7074d14c5d6c3ca35c51c
sigmashowspeed-crypto <sigmashowspeed@gmail.com>
2026-07-01 19:40:23 +0500

    add styles2 in Blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 // Abdurahman
 export default function Blog() {
   return (
-    <div className='bg-red-500 text-center'>Blog</div>
+    <div className='bg-red-500 text-center p-5 mt-5'>Blog</div>
   )
 }
